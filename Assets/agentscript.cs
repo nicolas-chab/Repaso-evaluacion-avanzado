74ce1307f63a5cb4559beefe65ec21586e9a8641
nicolas-chab <101563616+nicolas-chab@users.noreply.github.com>
2022-10-03 12:12:20 -0300

    Initial commit

---
+++ Assets/agentscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class agentscript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] Transform destinationtransform;
+    public float remainingDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        agent.destination = destinationtransform.position;
+        if (agent.isStopped==true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
